ceeff0e00beec05251a758decf500ad37475c83c
David Lazaro <dlazaro7@gmail.com>
2022-12-02 18:52:43 +0100

    details left aligned

--- phone-client/src/components/PhoneDetails.jsx
+++ phone-client/src/components/PhoneDetails.jsx
@@ -1,68 +1,58 @@
-import {useParams, useNavigate} from "react-router-dom"
-import {useState, useEffect} from "react"
-import { phonesDetailsService } from "../services/phones.services"
-import Spinner from 'react-bootstrap/Spinner';
-
-
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { phonesDetailsService } from "../services/phones.services";
+import Spinner from "react-bootstrap/Spinner";
 
 function PhoneDetails() {
-  
-const {id} = useParams()
-const navigate = useNavigate()
-console.log("🚀 ~ file: PhoneDetails.jsx:5 ~ PhoneDetails ~ id", id)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log("🚀 ~ file: PhoneDetails.jsx:5 ~ PhoneDetails ~ id", id);
 
-const [details, setdetails] = useState(null)
-const [isFetching, setIsFetching] = useState(true)
+  const [details, setdetails] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
 
-useEffect(() => {
+  useEffect(() => {
+    getData();
+  }, [id]);
 
-  getData()
+  const getData = async () => {
+    try {
+      const response = await phonesDetailsService(id);
+      console.log(
+        "🚀 ~ file: PhoneDetails.jsx:19 ~ getData ~ response",
+        response
+      );
+      setIsFetching(false);
+      setdetails(response.data);
+      navigate(`/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-}, [id])
-
-const getData = async () => {
-
-  try {
-    const response = await phonesDetailsService(id)
-    console.log("🚀 ~ file: PhoneDetails.jsx:19 ~ getData ~ response", response)
-    setIsFetching(false)
-    setdetails(response.data)
-    navigate(`/${id}`)
-  } catch (error) {
-    console.log(error)
+  if (isFetching === true) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
   }
-}
-
-if (isFetching === true) {
-  return (
-    <Spinner animation="border" role="status">
-    <span className="visually-hidden">Loading...</span>
-  </Spinner>
-    )
-}
 
   return (
     <div id="box-details" className="center">
-
-
-
-
-        <div style={{backgroundImage: `../../public/images/${details.imageFileName}`, width:"200px"}}></div>
-        <img src={`../../public/images/${details.imageFileName}`} alt="phone" />
-        <h2>{details.name}</h2>
-        <p>{details.description}</p>
-        <h3>{details.manufacturer}</h3>
-        <p>Price: {details.price}</p>
-        
-
-
-        
-
-
-
-
+      <img src={`../../public/images/${details.imageFileName}`} alt="phone" />
+      <h2>{details.name}</h2>
+      <p><b>Brand</b> {details.manufacturer}</p>
+      <p><b>Screen:</b> {details.screen}</p>
+      <p><b>Processor:</b> {details.processor} <b>Ram: {details.ram}</b></p>
+      <p><b>Color:</b> {details.color}</p>
+      <p><b>Price:</b> {details.price} €</p>
+      <p><b>Description:</b> {details.description}</p>
+      
+     
+      
     </div>
-  )
+  );
 }
 
-export default PhoneDetails
+export default PhoneDetails;
